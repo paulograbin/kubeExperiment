49994537ddf99c1a66cf59f167cdffc165927b57
Paulo Grabin <paulo.grabin@sap.com>
2025-12-12 14:13:09 -0300

    Add generated project

---
+++ src/main/java/com/paulograbin/kubeexperiment/KubeExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.paulograbin.kubeexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeExperimentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paulograbin/kubeexperiment/KubeExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulograbin.kubeexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
